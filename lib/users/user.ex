670f4993541c241d194838fbb9e64280bcc2275d
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-01-08 21:41:55 -0300

    feat: build user

---
+++ lib/users/user.ex
@@ -0,0 +1,21 @@
+defmodule Exlivery.Users.User do
+  @keys [:name, :email, :address, :cpf, :age]
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(name, email, address, cpf, age) when age >= 18 and is_bitstring(cpf) do
+    {
+      :ok,
+      %__MODULE__{
+        name: name,
+        email: email,
+        address: address,
+        cpf: cpf,
+        age: age
+      }
+    }
+  end
+
+  def build(_name, _email, _address, _cpf, _age), do: {:error, "Invalid parameters"}
+end
