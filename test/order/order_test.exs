2933f5d3c4d646d450444ad6edf0072b7abe83f1
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-01-10 22:29:11 -0300

    test: adding test in the module order

---
+++ test/order/order_test.exs
@@ -0,0 +1,40 @@
+defmodule Exlivery.Orders.OrderTest do
+  use ExUnit.Case
+
+  alias Exlivery.Orders.Order
+
+  import Exlivery.Factory
+
+  describe "build/2" do
+    test "when all params are valid, returns an order" do
+      user = build(:user)
+
+      items = [
+        build(:item),
+        build(:item,
+          description: "Cocada",
+          category: :sobremesa,
+          unity_price: Decimal.new("40"),
+          quantity: 3
+        )
+      ]
+
+      response = Order.build(user, items)
+
+      expected_response = {:ok, build(:order)}
+
+      assert response == expected_response
+    end
+
+    test "when there is not items in the order, returns an error" do
+      user = build(:user)
+      items = []
+
+      response = Order.build(user, items)
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+  end
+end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,7 +2,7 @@ defmodule Exlivery.Factory do
   use ExMachina
 
   alias Exlivery.Users.User
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Order, Item}
 
   def user_factory do
     %User{
@@ -15,11 +15,28 @@ defmodule Exlivery.Factory do
   end
 
   def item_factory do
-    %Exlivery.Orders.Item{
+    %Item{
       category: :pizza,
       description: "Pizza de queijo",
       quantity: 4,
-      unity_price: Decimal.new(30)
+      unity_price: Decimal.new("30")
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Algum lugar na terra",
+      items: [
+        build(:item),
+        build(:item,
+          category: :sobremesa,
+          description: "Cocada",
+          quantity: 3,
+          unity_price: Decimal.new("40")
+        )
+      ],
+      total_price: Decimal.new("240.00"),
+      user_cpf: "123456789"
     }
   end
 end
